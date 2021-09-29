f007f06b575b564f4639d6b87a27eae56353e876
dialectrical <michael.camele@gmail.com>
2021-09-29 15:15:45 -0700

    feat: styled StyledHeader

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-import logo from "./logo.svg";
-import "./App.css";
+import { StyledHeader } from "./components/styles/StyledHeader";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <StyledHeader>Publish your podcasts everywhere</StyledHeader>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/styles/StyledHeader.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const StyledHeader = styled.h2`
+  color: var(--primary-color);
+  font-size: 26px;
+  font-weight: 300;
+  font-style: normal;
+  line-height: 38px;
+  text-align: center;
+  text-transform: uppercase;
+`;
