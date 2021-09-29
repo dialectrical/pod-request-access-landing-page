3497880b2222132b727fc7bbfb7e5b9816ddf046
dialectrical <michael.camele@gmail.com>
2021-09-29 16:57:45 -0700

    fix: fixed text styling inside StyledTextInput

--- src/components/styles/StyledTextInput.js
+++ src/components/styles/StyledTextInput.js
@@ -1,12 +1,17 @@
 import styled from "styled-components";
 
 export const StyledTextInput = styled.input`
+  display: block;
   background-color: var(--background-secondary-color);
-  color: var(--content-color);
+  color: rgba(255, 255, 255, 0.5);
   width: 327px;
   height: 46px;
   border: none;
   border-radius: 28px;
+  font-family: "Chivo";
   font-size: 14px;
+  font-weight: 400;
   line-height: 28px;
+  text-indent: 32px;
+  margin: auto;
 `;
