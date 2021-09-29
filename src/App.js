51201816742e9b9c86a1909719af09912e601f60
dialectrical <michael.camele@gmail.com>
2021-09-29 15:17:39 -0700

    feat: styled StyledSubHeader

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import { StyledHeader } from "./components/styles/StyledHeader";
+import { StyledSubHeader } from "./components/styles/StyledSubHeader";
 
 function App() {
   return (
     <div className="App">
-      <StyledHeader>Publish your podcasts everywhere</StyledHeader>
+      <StyledHeader>
+        Publish your podcasts <StyledSubHeader>everywhere.</StyledSubHeader>
+      </StyledHeader>
     </div>
   );
 }
---
+++ src/components/styles/StyledSubHeader.js
@@ -0,0 +1,5 @@
+import styled from "styled-components";
+
+export const StyledSubHeader = styled.span`
+  color: var(--secondary-color);
+`;
